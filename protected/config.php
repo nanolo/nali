6a55ea9436f2314750e044daf37d8dc550c5f70e
vincent <vincent@localhost.localdomain>
2019-10-13 14:04:35 +0300

    case for config.php added

---
+++ protected/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'db' => [
+        'driver' => '{{dbdriver}}',
+        'host' => '{{dbhost}}',
+        'user' => '{{dbuser}}',
+        'password' => '{{dbpassword}}',
+        'dbname' => '{{dbname}}'
+    ]
+]
